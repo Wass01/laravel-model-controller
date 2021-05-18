8add3106289094333a226f8298533cfedd51c3aa
wass01 <maatallawassim01@gmail.com>
2021-05-18 17:48:07 +0200

    esercizio finito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Movie</title>
 </head>
 <body>
-  ciaoaoaoa
-
+  @foreach ($movies as $movie)
+        <div>
+            <h1>Titolo: {{$movie->title}}</h1>
+            <h2>Titolo originale:{{$movie->original_title}}</h2>
+            <p>Nazionalità: {{$movie->nationality}}</p>
+            <p>Data: {{$movie->date}}</p>
+            <p>Voto: {{$movie->vote}}/10</p>
+        </div>
+    @endforeach
 </body>
 </html>
